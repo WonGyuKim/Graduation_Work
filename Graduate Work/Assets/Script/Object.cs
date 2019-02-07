f25671afab9395787f19688d4e06b466d678f8ec
Kim Won Gyu <m55f@naver.com>
2019-02-07 11:56:59 +0900

    Start the Project

---
+++ Graduate Work/Assets/Script/Object.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x + Screen.width / 2, Camera.main.transform.position.y + Screen.height / 2, 10));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if (Input.GetMouseButton(0))
+        //{
+        //    transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        //}
+
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+    }
+}
