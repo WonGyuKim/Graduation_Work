45cf305852e0c6a48b45c59900a483902dccfe15
Kim Won Gyu <m55f@naver.com>
2019-02-08 15:33:25 +0900

    Finish Setting All Prefabs

--- Graduate Work/Assets/Script/Object.cs
+++ Graduate Work/Assets/Script/Object.cs
@@ -7,22 +7,42 @@ public class Object : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x + Screen.width / 2, Camera.main.transform.position.y + Screen.height / 2, 10));
+        if (transform.parent != null)
+        {
+
+            Transform parent = transform;
+            while (parent.transform.parent != null)
+            {
+                parent = parent.transform.parent;
+            }
+            parent.position = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x + Screen.width / 2, Camera.main.transform.position.y + Screen.height / 2, 10));
+        }
+        else        
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x + Screen.width / 2, Camera.main.transform.position.y + Screen.height / 2, 10));
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        //if (Input.GetMouseButton(0))
-        //{
-        //    transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
-        //}
-
+        
     }
 
     void OnMouseDrag()
     {
-        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        //transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        if (transform.parent != null)
+        {
+            Transform parent = transform;
+            while (parent.transform.parent != null)
+            {
+                parent = parent.transform.parent;
+            }
+            parent.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        }
+        else
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+
     }
 }
