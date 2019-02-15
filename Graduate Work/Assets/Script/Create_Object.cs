a24871b45da1925b3c46a2e087a6a19526b06e18
Kim Won Gyu <m55f@naver.com>
2019-02-15 17:34:27 +0900

    Add UI Interface

---
+++ Graduate Work/Assets/Script/Create_Object.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create_Object : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public GameObject[] Pool;
+    private static int target = 0;
+
+    public void Action()
+    {
+        // Instantiate(Pool[0]);
+        string path = "Models/Prefabs/";
+        UnityEngine.Object obj = Resources.Load(path + "15gear");
+        Debug.Log("====================");
+        Debug.Log(obj);
+        Debug.Log(target);
+        Debug.Log("====================");
+        Instantiate((GameObject)obj);
+        //GameObject data = Instantiate(Resources.Load(path + "15gear")) as GameObject;
+    }
+
+    public void DropDownList()
+    {
+        UnityEngine.UI.Dropdown dropdown = GetComponent<UnityEngine.UI.Dropdown>();
+        target = dropdown.value;
+        Debug.Log(target);
+        Debug.Log(dropdown.value);
+    }
+}
