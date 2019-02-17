6518cf342b39c23a27a758f2b27f7338a20d7399
KimWonGyu <m55f@naver.com>
2019-02-18 01:07:05 +0900

    change on UI script

--- Graduate Work/Assets/Script/UIManager.cs
+++ Graduate Work/Assets/Script/UIManager.cs
@@ -1,35 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     // UI Components
-    BtnCreate BtnCreate;
-    DDLCreate DDLCreate;
+    public Button BtnCreate;
+    public Dropdown DDLCreate;
 
     // Variables
-    private static int target;
+    private int target;
 
-    private void Start()
+    enum Object
     {
-        // Init UI Components
-        BtnCreate = gameObject.AddComponent<BtnCreate>();
-        DDLCreate = gameObject.AddComponent<DDLCreate>();
+        gear15,
+        gear30,
+        axle1,
+        axle2,
+        axle4,
+        beam3,
+        beam6,
+        beam12,
+        connector1,
+        connector2,
+        motor,
+        motorbody
+    };
 
-        // Init Variables
-        //target = DDLCreate.Value;
-        target = 0;
+    public void Start()
+    {
     }
 
     public void CreateObject()
     {
-        BtnCreate.Action(target);
+        GameObject obj = MakeObject(target);
+
+        Instantiate(obj);
     }
 
     public void GetObjectValue()
     {
-        target = DDLCreate.Value;
-        Debug.Log(target);
+        target = DDLCreate.value;
+    }
+
+    /* 
+     * Get input number and Return appropriate Object
+     * You should change this function to adjust for making Objects
+     */
+    private GameObject MakeObject(int target)
+    {
+        string path = "Models/Prefabs/";
+        string ObjectName = "";
+
+        switch ((Object)target)
+        {
+            case Object.gear15:
+                ObjectName = Object.gear15.ToString();
+                break;
+            case Object.gear30:
+                ObjectName = Object.gear30.ToString();
+                break;
+            case Object.axle1:
+                ObjectName = Object.axle1.ToString();
+                break;
+            case Object.axle2:
+                ObjectName = Object.axle2.ToString();
+                break;
+            case Object.axle4:
+                ObjectName = Object.axle4.ToString();
+                break;
+            case Object.beam3:
+                ObjectName = Object.beam3.ToString();
+                break;
+            case Object.beam6:
+                ObjectName = Object.beam6.ToString();
+                break;
+            case Object.beam12:
+                ObjectName = Object.beam12.ToString();
+                break;
+            case Object.connector1:
+                ObjectName = Object.connector1.ToString();
+                break;
+            case Object.connector2:
+                ObjectName = Object.connector2.ToString();
+                break;
+        }
+
+        UnityEngine.GameObject obj = Resources.Load(path + ObjectName) as GameObject;
+
+        return obj;
     }
 }
