691ddae25c167128f2aee4f7667bd9fd09df3b84
KimKwangHwan <https://github.com/KimKwangHwan>
2019-03-01 00:00:01 +0900

    Commit File

---
+++ Graduate Work/Assets/Script/Object.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform.parent != null)
+        {
+            Transform parent = transform;
+
+            while (parent.transform.parent != null)
+            {
+                parent = parent.transform.parent;
+            }
+            parent.position = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x + Screen.width / 2, Camera.main.transform.position.y + Screen.height / 2, 2));
+        }
+        else        
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x + Screen.width / 2, Camera.main.transform.position.y + Screen.height / 2, 2));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDrag()
+    {
+        //transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        if (transform.parent != null)
+        {
+            Transform parent = transform;
+            while (parent.transform.parent != null)
+            {
+                parent = parent.transform.parent;
+            }
+            parent.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 2));
+        }
+        else
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 2));
+
+    }
+}
