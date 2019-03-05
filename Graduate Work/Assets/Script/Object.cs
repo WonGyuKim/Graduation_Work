a2c6b9c7c9d75128e7a1a96676ae700b11eab565
club room <m55f@naver.com>
2019-03-05 20:33:51 +0900

    fix the fucking conflict

--- Graduate Work/Assets/Script/Object.cs
+++ Graduate Work/Assets/Script/Object.cs
@@ -15,17 +15,17 @@ public class Object : MonoBehaviour
             {
                 parent = parent.transform.parent;
             }
-<<<<<<< HEAD
-            parent.position = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x + Screen.width / 2, Camera.main.transform.position.y + Screen.height / 2, 2));
-        }
-        else        
+//<<<<<<< HEAD
+            //parent.position = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x + Screen.width / 2, Camera.main.transform.position.y + Screen.height / 2, 2));
+        
+        /*else        
             transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x + Screen.width / 2, Camera.main.transform.position.y + Screen.height / 2, 2));
-=======
+=======*/
             parent.position = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x + Screen.width / 2, Camera.main.transform.position.y + Screen.height / 2, 10));
         }
         else        
             transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.transform.position.x + Screen.width / 2, Camera.main.transform.position.y + Screen.height / 2, 10));
->>>>>>> fc084b894c8c4f5c063d2b44f3c55f26483f076c
+//>>>>>>> fc084b894c8c4f5c063d2b44f3c55f26483f076c
         
     }
 
@@ -45,17 +45,17 @@ public class Object : MonoBehaviour
             {
                 parent = parent.transform.parent;
             }
-<<<<<<< HEAD
+/*<<<<<<< HEAD
             parent.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 2));
         }
         else
             transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 2));
-=======
+=======*/
             parent.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
         }
         else
             transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
->>>>>>> fc084b894c8c4f5c063d2b44f3c55f26483f076c
+//>>>>>>> fc084b894c8c4f5c063d2b44f3c55f26483f076c
 
     }
 }
