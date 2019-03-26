d115ec7bb9d537e2c47c424d13a0a65845a51d2b
Kim Won Gyu <m55f@naver.com>
2019-03-26 17:59:37 +0900

    delete unnecessary lines

--- Graduate Work/Assets/Script/Object.cs
+++ Graduate Work/Assets/Script/Object.cs
@@ -8,7 +8,6 @@ public class Object : MonoBehaviour
     GameObject plane;
     GameObject sphere;
     Transform parent;
-    Quaternion temp_r;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +15,6 @@ public class Object : MonoBehaviour
         origin = new Vector3();
         plane = GameObject.Find("Plane");
         parent = transform;
-        temp_r = parent.rotation;
 
         if (parent.parent != null)
         {
@@ -38,15 +36,6 @@ public class Object : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftControl))
         {
             
-            //sphere.AddComponent<SphereCollider>();
-            
-            /*
-            origin = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, plane.transform.position.z));
-            
-            origin.x -= parent.position.x;
-            origin.y -= parent.position.y;
-            origin.z -= parent.position.z;
-            */
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit rayhit;
             if (Physics.Raycast(ray, out rayhit))
@@ -58,6 +47,7 @@ public class Object : MonoBehaviour
                 temp.transform.position = parent.transform.position;
                 sphere.transform.parent = parent;
                 sphere.transform.rotation = parent.rotation;
+
             }
 
             if (Physics.Raycast(ray, out rayhit) && rayhit.collider.gameObject.Equals(sphere))
@@ -66,18 +56,7 @@ public class Object : MonoBehaviour
             }
 
         }
-        else if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(90f, 0, 0);
-        }
-        else if (Input.GetKey(KeyCode.W))
-        {
-            transform.Rotate(0, 90f, 0);
-        }
-        else if (Input.GetKey(KeyCode.E))
-        {
-            transform.Rotate(0, 0, 90f);
-        }
+        
     }
 
     private void OnMouseDrag()
@@ -88,7 +67,7 @@ public class Object : MonoBehaviour
             Vector3 click;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit rayhit;
-
+            /*
             if (Physics.Raycast(ray, out rayhit) && rayhit.collider.gameObject.Equals(sphere))
             {
                 click = rayhit.point - parent.position;
@@ -98,15 +77,12 @@ public class Object : MonoBehaviour
                 //click = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x - transform.position.x, Input.mousePosition.y - transform.position.y, plane.transform.position.z - transform.position.z));
                 click = rayhit.point - parent.position;
             }
+            */
+            Physics.Raycast(ray, out rayhit);
+            click = rayhit.point - parent.position;
 
             // arcball rotation
-            Debug.Log("Origin : (x, y, z) = (" + origin.x + ", " + origin.y + ", " + origin.z + ")");
-            Debug.Log("Dynamic : (x, y, z) = (" + click.x + ", " + click.y + ", " + click.z + ")");
-
-
             // Step_1. 
-            //float V1 = Mathf.Sqrt(origin.x * origin.x + origin.y * origin.y + origin.z * origin.z);
-            //float V2 = Mathf.Sqrt(click.x * click.x + click.y * click.y + click.z * click.z);
             float Scale = click.magnitude / origin.magnitude;
             
             // Step_2.
@@ -115,8 +91,6 @@ public class Object : MonoBehaviour
             float InPro = ScaledV1.x * click.x + ScaledV1.y * click.y + ScaledV1.z * click.z;
             //InPro /= V1 * V2;
             float angle = Mathf.Acos(InPro);
-            /*if (angle < 0)
-                angle = Mathf.PI / 2 - angle;*/
 
             // Step_3.
             // Cross Product
@@ -125,9 +99,6 @@ public class Object : MonoBehaviour
                                             ScaledV1.x * click.y - ScaledV1.y * click.x);
 
             // Step_4. Now We can make conclusion Rotation by Quaternion
-            //Quaternion rotation = new Quaternion(CroPro.x, CroPro.y, CroPro.z, angle);
-            //Debug.Log(rotation);
-            //parent.rotation = new Quaternion(parent.rotation.x + CroPro.x, parent.rotation.y + CroPro.y, parent.rotation.z + CroPro.z, parent.rotation.w + angle);
             parent.rotation = new Quaternion(parent.rotation.x + CroPro.x * Mathf.Sin(angle / 2), parent.rotation.y + CroPro.y * Mathf.Sin(angle / 2), parent.rotation.z + CroPro.z * Mathf.Sin(angle / 2), parent.rotation.w + Mathf.Cos(angle));
 
         }
@@ -143,6 +114,5 @@ public class Object : MonoBehaviour
     private void OnMouseUp()
     {
         Destroy(sphere);
-        temp_r = parent.rotation;
     }
 }
