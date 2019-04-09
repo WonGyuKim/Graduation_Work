87d76b654e45c68d67ebdb5d9f6eb36c163c4962
KimKwangHwan <louie1004@naver.com>
2019-04-09 12:20:07 +0900

    re0409kkh

---
+++ Graduate Work/Assets/Script/temp1/Parts.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[Serializable]
+public class Parts
+{
+    
+}
