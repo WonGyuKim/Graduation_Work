c2de59f515f04e372a8ce31903ecc036272a4360
KimKwangHwan <louie1004@naver.com>
2019-05-08 15:51:47 +0900

    New Interface Project

---
+++ Graduate Work/Assets/Script/Interface.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interface : MonoBehaviour
+{
+    
+}
+
+public interface IParts
+{
+    void Link(Transform hole, Transform otherTrans);
+
+    void LinkMove(Transform hole, Transform otherTrans);
+
+    void LinkExit(Transform hole, Transform otherTrans);
+
+    void VerticalMove();
+
+    void MouseMove();
+
+    void ArcballMove();
+
+    void MotoringMove();
+
+    bool OnDragCheck
+    {
+        get;
+    }
+}
