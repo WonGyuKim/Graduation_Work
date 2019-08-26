f73ad55285b96091c7ceed2b8ff9001832dfc8f3
Kim <m55f@naver.com>
2019-08-26 18:00:24 +0900

    kwg_12

---
+++ Graduate Work/Assets/Script/KWG_Script/Link.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    public void Connect()
+    {
+
+    }
+
+    public void Disconnect()
+    {
+
+    }
+
+    public virtual void LinkRotation() {}
+}
