1bcdbc61a7a80c14503f50510d6f2b0fedc53ad6
Kim <m55f@naver.com>
2019-08-28 18:01:02 +0900

    kwg_13

--- Graduate Work/Assets/Script/KWG_Script/Link.cs
+++ Graduate Work/Assets/Script/KWG_Script/Link.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Link : MonoBehaviour
 {
+    MyParts left;
+    MyParts right;
+
     public void Connect()
     {
 
@@ -14,5 +17,21 @@ public class Link : MonoBehaviour
 
     }
 
-    public virtual void LinkRotation() {}
+    public virtual void LinkRotation() { }
+
+    /*
+     * 동력이 왼쪽에서 오는지 어떻게 아는가?
+     * Parent가 판단
+     * 
+     */
+    public MyParts Left
+    {
+        get { return left; }
+        set { left = value; }
+    }
+    public MyParts Right
+    {
+        get { return right; }
+        set { right = value; }
+    }
 }
